2869bbbe27f764bed78144c399f6b45584f9857f
tonklis <benjamin.hernandez@in2teck.com>
2012-02-06 12:43:56 -0600

    Se agrego formato y textos para edicion y creacion de entradas

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -2,18 +2,22 @@ class UsuariosController < ApplicationController
   before_filter :authenticate_usuario!
 
   def index
+		valida_usuario
     @usuarios = Usuario.all
   end
 
   def show
+		valida_usuario
     @usuario = Usuario.find(params[:id])
   end
 
   def new
+		valida_usuario
     @usuario = Usuario.new
   end
 
   def create
+		valida_usuario
     @usuario = Usuario.new(params[:usuario])
     if @usuario.save
       redirect_to @usuario, :notice => "Successfully created usuario."
@@ -23,10 +27,12 @@ class UsuariosController < ApplicationController
   end
 
   def edit
+		valida_usuario
     @usuario = Usuario.find(params[:id])
   end
 
   def update
+		valida_usuario
     @usuario = Usuario.find(params[:id])
     if @usuario.update_attributes(params[:usuario])
       redirect_to @usuario, :notice  => "Successfully updated usuario."
@@ -36,8 +42,17 @@ class UsuariosController < ApplicationController
   end
 
   def destroy
+		valida_usuario
     @usuario = Usuario.find(params[:id])
     @usuario.destroy
     redirect_to usuarios_url, :notice => "Successfully destroyed usuario."
   end
+
+	def valida_usuario
+		usuario = Usuario.find_by_twitter_id(session["devise.uid"])
+		if not usuario.email == "tonklis"
+			redirect_to entries_url, :notice => "Invalid user."
+		end
+	end
+
 end
